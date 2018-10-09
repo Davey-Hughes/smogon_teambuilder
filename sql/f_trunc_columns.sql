893b4ec6917b743f955cb487c6ba33b35b1918da
davey <davidralphhughes@college.harvard.edu>
2018-10-09 05:17:51 -0400

    Adds varchar[] truncating to f_trunc_columns

--- sql/f_trunc_columns.sql
+++ sql/f_trunc_columns.sql
@@ -1,4 +1,9 @@
 --  from https://stackoverflow.com/a/34446754
+--
+--  for a varchar[], this uses array_to_string() to convert the array to text,
+--  and then inserts that as the first value of an array to keep the type. The
+--  max length of the inner text, then, is the _len -4, since printing the
+--  array will add {} and "" to the output
 CREATE OR REPLACE FUNCTION f_trunc_columns(_tbl anyelement, _len int = 80)
   RETURNS SETOF anyelement AS
 $func$
@@ -9,6 +14,10 @@ BEGIN
    SELECT format('SELECT %s FROM %s'
                , string_agg(CASE WHEN a.atttypid = 'text'::regtype  -- simple case text
                               THEN format('left(%I, %s)', a.attname, _len)
+                            WHEN a.atttypid = 'varchar[]'::regtype
+                              THEN format(
+                                'ARRAY[left(array_to_string(%I, '', ''), %s)]::varchar[]'
+                                 , a.attname, _len - 4, format_type(a.atttypid, a.atttypmod))
                             WHEN a.atttypid = ANY(_typ)             -- other short types
                               THEN format('left(%I::text, %s)::%s'
                                  , a.attname, _len, format_type(a.atttypid, a.atttypmod))
